b8e3da733f548b8dd72ce0eacf58c351b4ad57be
leon segal <leonsegal1@gmail.com>
2022-05-14 23:49:55 +0100

    add player identifier

--- resources/js/Pages/Messages.js
+++ resources/js/Pages/Messages.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-export default function Messages({ messages }) {
+export default function Messages({ messages, user }) {
     return (
         <ul>
             {messages.map((message) => (
                 <li key={message.id}>
-                    {message.player_name}: {message.body}
+                    {message.player_name}
+                    {user.id === message.player_id ? "(you): " : ": "}
+                    {message.body}
                 </li>
             ))}
         </ul>
