a03679a944a4e37357faf9c5c506d1f0789529f2
Leon Segal <leonsegal1@gmail.com>
2022-05-27 23:19:15 +0100

    amend player display

--- resources/js/Pages/header/RoleInfo.js
+++ resources/js/Pages/header/RoleInfo.js
@@ -1,15 +1,14 @@
 import React from "react";
 
 export default function RoleInfo({ roles, player }) {
+    let otherRoles = roles.filter((role) => role.id !== player.role_id);
+    let roleNames = otherRoles.map((role) => role.name);
+
     return (
         <div id="roles">
             <strong>Roles: </strong>
 
-            {roles
-                .filter((role) => role.id !== player.role_id)
-                .map((role, id) => (
-                    <span key={id}>{role.name} </span>
-                ))}
+            {roleNames.join(" - ")}
         </div>
     );
 }
